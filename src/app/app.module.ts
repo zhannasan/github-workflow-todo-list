71aa044f952ec5cd45e86f1a1a28024f25a7e08c
Zhanna <j.santybay@gmail.com>
2020-02-11 11:36:55 +0100

    calendrier #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListerTodosComponent } from './lister-todos/lister-todos.component';
+import { CalendrierComponent } from './calendrier/calendrier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListerTodosComponent,
+    CalendrierComponent
   ],
   imports: [
     BrowserModule
