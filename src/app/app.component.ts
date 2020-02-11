ae9163406a11cbebb4964a18b29e5cb004bc24c2
Audrey Martin <audrey.martin26@gmail.com>
2020-02-11 13:23:44 +0100

    enlever conflit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,5 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 })
 export class AppComponent {
   title = 'todo-list';
-<<<<<<< HEAD
-
-=======
   calendarPlugins = [dayGridPlugin];
->>>>>>> eaed9cf88a7751f33ffcb0f6f48644c29e4404e0
 }
