c6267f14c83a3618ebf5299d8550697245c5aa4a
Julien Dewost <dewostjulien@gmail.com>
2020-02-11 11:12:42 +0100

    menu-todo : #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ListerTodosComponent } from './lister-todos/lister-todos.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListerTodosComponent
+    ListerTodosComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
